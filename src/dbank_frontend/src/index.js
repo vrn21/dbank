cd654ffa95aec91af1628905b4fdd488c90a5ab2
vrn21 <varunkkv.mec@gmail.com>
2023-07-02 11:53:02 +0530

    Temporarily added node and express tags

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,4 +1,6 @@
 import { dbank_backend } from "../../declarations/dbank_backend";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -20,7 +22,35 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   return false;
 });
 
-document.getElementById("login").addEventListener("click", function() {
-  // Replace "target-page.html" with the actual URL or file path of the page you want to open
-  window.location.href = "login.html";
-});
+// document.getElementById("login").addEventListener("click", function() {
+//   // Replace "target-page.html" with the actual URL or file path of the page you want to open
+//   window.location.href = "login.html";
+// });
+// document.getElementById("signup").addEventListener("click", function() {
+//   // Replace "target-page.html" with the actual URL or file path of the page you want to open
+//   window.location.href = "signup.html";
+  
+// });
+
+
+//node 
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"../src/index.html");
+})
+
+app.get("/login",function(req,res){
+  res.sendFile(__dirname+"/login.html");
+})
+
+app.get("/signup",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+})
+
+app.listen(8080,function(){
+  console.log("Started serving 3000");
+})
